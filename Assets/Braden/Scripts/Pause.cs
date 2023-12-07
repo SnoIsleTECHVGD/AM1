3b69c62b652fe166bb7051f345cc3d2e446b4dca
Nichols <1719263@mukilteo.wednet.edu>
2023-12-07 10:17:13 -0800

    add PauseSystem v1

---
+++ Assets/Braden/Scripts/Pause.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public bool isPaused = false;
+    public KeyCode pauseKey = KeyCode.Escape;
+
+    public Canvas mainCanvas;
+    public Canvas bgCanvas;
+
+    void Start()
+    {
+        mainCanvas.enabled = false;
+        bgCanvas.enabled = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            SetPaused(!isPaused);
+        }
+    }
+
+    // Main
+
+    public void SetPaused(bool state)
+    {
+        if (state == isPaused)
+            return;
+
+        isPaused = state;
+
+        mainCanvas.enabled = state;
+        bgCanvas.enabled = state;
+
+        if (state == true)
+        {
+            Time.timeScale = 0;
+        } else
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
