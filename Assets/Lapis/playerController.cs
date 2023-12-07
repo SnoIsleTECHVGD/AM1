40601b44b2174752eaeff34beb58144939e14c2b
Krueger <1717919@mukilteo.wednet.edu>
2023-12-07 10:09:12 -0800

    player controller update

--- Assets/Lapis/playerController.cs
+++ Assets/Lapis/playerController.cs
@@ -7,7 +7,7 @@ public class playerController : MonoBehaviour
     public float buildUp;
     public float maxSpeed;
     public float jumpspeed;
-
+    private int jumpCount = 1;
     private Rigidbody2D pc;
     // Start is called before the first frame update
     void Start()
@@ -18,19 +18,25 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.A) == true)
+        if (Input.GetKey(KeyCode.A))
         {
             pc.AddForce(Vector2.left * buildUp);
         }
-        if (Input.GetKey(KeyCode.D) == true)
+        if (Input.GetKey(KeyCode.D))
         {
             pc.AddForce(Vector2.right * buildUp);
         }
-        if (Input.GetKeyDown(KeyCode.Space) == true)
+        if (Input.GetKeyDown(KeyCode.Space) && jumpCount == 1)
         {
+            jumpCount--;
             pc.AddForce(Vector2.up * jumpspeed, ForceMode2D.Impulse);
         }
         pc.velocity = new Vector2(Mathf.Clamp(pc.velocity.x, -maxSpeed, maxSpeed), pc.velocity.y);
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        jumpCount = 1;
     }
 
 }
