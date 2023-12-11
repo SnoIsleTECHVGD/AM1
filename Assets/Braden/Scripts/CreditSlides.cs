47e178c08edb111972f3bf009aef86cc4ecf4467
Nichols <1719263@mukilteo.wednet.edu>
2023-12-11 09:46:18 -0800

    add credits, needs images though

---
+++ Assets/Braden/Scripts/CreditSlides.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CreditSlides : MonoBehaviour
+{
+    private int slideNumber = 0;
+    private int amountOfSlides = 6;
+
+    private Image currentSlide;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DisplayNextSlide(Random.Range(1, 6));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D))
+            DisplayNextSlide(1);
+        else if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A))
+            DisplayNextSlide(-1);
+    }
+
+    // Main
+    public void DisplayNextSlide(int difference)
+    {
+        slideNumber += difference;
+
+        if (slideNumber > amountOfSlides)
+            slideNumber = 1;
+        else if (slideNumber <= 0)
+            slideNumber = amountOfSlides;
+
+        if (currentSlide)
+            currentSlide.enabled = false;
+
+        GameObject findObject = GameObject.Find("Person" + slideNumber.ToString());
+        currentSlide = findObject.GetComponent<Image>();
+
+        currentSlide.enabled = true;
+    }
+}
