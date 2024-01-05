4c09fea591052bf9af60abf164584d2e5c8e442f
Krueger <1717919@mukilteo.wednet.edu>
2024-01-05 08:36:20 -0800

    oh man i sure hope no merge conflicts happen (clueless)

--- Assets/buttonPush.cs
+++ Assets/buttonPush.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class buttonPush : MonoBehaviour
 {
+    public GameObject pillar;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +20,19 @@ public class buttonPush : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PushObject"))
         {
+            Debug.Log("pushed");
             //dothething
+            pillar.transform.Translate(Vector3.up);
+        }
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("PushObject"))
+        {
+            Debug.Log("unpushed");
+            //undothething
+            pillar.transform.Translate(Vector3.down);
         }
     }
 }
