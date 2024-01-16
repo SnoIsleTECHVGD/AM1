3f3d4bdad9ec7104c2caac36d8590bc3a070f882
Nichols <1719263@mukilteo.wednet.edu>
2024-01-16 09:45:54 -0800

    revamped button

--- Assets/Lapis/Scripts/buttonPush.cs
+++ Assets/Lapis/Scripts/buttonPush.cs
@@ -5,15 +5,55 @@ using UnityEngine;
 public class buttonPush : MonoBehaviour
 {
     public GameObject pillar;
+
     public Vector3 changeVector;
+    public float lerpSpeed;
+
+    private Vector3 basePosition;
+    private Vector3 goal = Vector3.zero;
+
+    private float dt;
+
+    private bool currentCollide = false;
+    private bool isColliding = false;
+
+    private void Start()
+    {
+        basePosition = pillar.transform.position;
+    }
+
+    private void Update()
+    {
+        if (goal == Vector3.zero)
+            return;
+
+        dt = Mathf.Clamp((Time.deltaTime / lerpSpeed) + dt, 0, 1);
+        pillar.transform.position = Vector3.Lerp(basePosition, basePosition + goal, dt);
+
+        if (dt == 1 && isColliding != currentCollide)
+        {
+            currentCollide = isColliding;
+            basePosition = pillar.transform.position;
+
+            goal = -goal;
+            dt = 0;
+        } 
+    }
+
+    // Events
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("PushObject"))
         {
-            Debug.Log("pushed");
             //dothething
-            pillar.transform.Translate(changeVector);
+            isColliding = true;
+
+            if (goal == Vector3.zero)
+            {
+                currentCollide = true;
+                goal = changeVector;
+            }
         }
 
     }
@@ -21,9 +61,8 @@ public class buttonPush : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("PushObject"))
         {
-            Debug.Log("unpushed");
             //undothething
-            pillar.transform.Translate(-changeVector);
+            isColliding = false;
         }
     }
 }
